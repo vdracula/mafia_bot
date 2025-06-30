ffebdb5f104cbe8a8fd16b20bed49c0314211267
Vladislav Kvasilchuk <vladkvi88@mail.ru>
2025-07-01 00:43:46 +0300

    new bot1

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 import os
 
-DATABASE_URL = "postgresql://postgres:bralbJRNotAyduqqaJrVSslZBTrOYbmC@postgres.railway.internal:5432/railway"
+DATABASE_URL = "postgresql+asyncpg://postgres:bralbJRNotAyduqqaJrVSslZBTrOYbmC@postgres.railway.internal:5432/railway"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
